c54729c0329eed41650d8cdb72100eeb3c37edd2
silence <beast964089@gmail.com>
2022-06-28 11:08:46 +0800

    fix error

--- pages/index.jsx
+++ pages/index.jsx
@@ -14,6 +14,7 @@ const data = {
               content: [
                 {
                   body: "PM",
+                  content: []
                 },
                 {
                   body: "RD",

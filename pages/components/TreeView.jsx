e5331dd03c96391eb933d517313f43be4a22fc2a
silence <beast964089@gmail.com>
2022-06-28 11:05:02 +0800

    修改結構

--- pages/components/TreeView.jsx
+++ pages/components/TreeView.jsx
@@ -1,44 +1,20 @@
 import React from 'react';
-const TreeView = () => {
-    return (
-        <ul>
-            <li className="root">執行長
-                <ul>
-                    <li>研發部
-                        <ul>
-                            <li>產品團隊
-                                <ul>
-                                    <li>專案經理</li>
-                                    <li>系統分析</li>
-                                    <li>開發工程師</li>
-                                </ul>
 
-                            </li>
-                            <li>專案團隊
-                                <ul>
-                                    <li>專案一部
-                                        <ul>
-                                            <li>PM</li>
-                                            <li>RD</li>
-                                        </ul>
-                                    </li>
-                                    <li>專案二部
-                                        <ul>
-                                            <li>PM</li>
-                                            <li>RD</li>
-                                        </ul>
-                                    </li>
-                                </ul>
-                            </li>
-                            <li>訓練團隊
-                            </li>
-                        </ul>
-                    </li>
-                    <li>業務團隊</li>
+const TreeView = (props) => {
+    const { data } = props
+    if (data.content) {
+        return (
+            <li>
+                {data.body}
+                <ul>
+                    {data.content.map(el =>
+                        <li key={el.body}>
+                            <TreeView data={el} />
+                        </li>
+                    )}
                 </ul>
             </li>
-        </ul>
-    );
+        )
+    }
 }
-
 export default TreeView;

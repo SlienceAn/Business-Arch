a02c23c784d9e524952d2084f3bc53c74c34eab4
silence <beast964089@gmail.com>
2022-08-08 17:46:32 +0800

    新增Context

--- components/ToolHead.jsx
+++ components/ToolHead.jsx
@@ -1,38 +1,49 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import InputModal from './InputModal';
+import { Context } from '../public/lib';
+import { BsPlusCircleFill } from 'react-icons/bs';
 
 const ToolHead = ({ parentTitle }) => {
     const [childVal, setChildVal] = useState([])
     const [parentVal, setParentVal] = useState("")
-    const getValue = (event, cate) => {
+    const [context, setContext] = useContext(Context)
+
+    const getValue = (event, cate, idx = 0) => {
         if (cate === "select") {
             setParentVal(event.target.value)
         }
         if (cate === "input") {
-            setChildVal(event.target.value)
+            const arr = [...childVal]
+            arr[idx]['body'] = event.target.value;
+            setChildVal(arr)
         }
     }
-    //test
     const postData = () => {
         const obj = {
             body: parentVal,
-            content: [{
-                body: childVal,
-                content: []
-            }]
+            content: childVal
         }
+        setContext(obj)
     }
     return (
         <>
             <div className='w-100 py-2 px-4 d-flex justify-content-between'>
                 <div className='d-flex gap-2'>
-                    <button className='btn btn-success' data-bs-toggle="modal" data-bs-target="#exampleModal">
+                    <button className='btn btn-success d-flex gap-2' data-bs-toggle="modal" data-bs-target="#exampleModal">
                         <i className="bi bi-pencil" />
                         <span>添加</span>
                     </button>
-                    <button className='btn btn-danger'>
+                    <button className='btn btn-danger d-flex gap-2'>
                         <i className="bi bi-x-square-fill" />
                         <span>刪除</span>
+                    </button>
+                    <button className='btn btn-info d-flex gap-2'>
+                        <i className="bi bi-x-square-fill" />
+                        <span>存檔</span>
+                    </button>
+                    <button className='btn btn-info d-flex gap-2'>
+                        <i className="bi bi-x-square-fill" />
+                        <span>匯出圖片</span>
                     </button>
                 </div>
                 <input placeholder='search' />
@@ -46,8 +57,14 @@ const ToolHead = ({ parentTitle }) => {
                     </select>
                 </div>
                 <div className='mb-3'>
-                    <label className='form-label' htmlFor='childItem'>添加子項</label>
-                    <input id='childItem' className='form-control' onChange={(event) => getValue(event, "input")} />
+                    <label className='form-label d-flex align-content-end gap-2' htmlFor='childItem'>
+                        <span>添加子項</span>
+                        <BsPlusCircleFill fontSize="1.5rem" />
+                    </label>
+                    {
+                        childVal.map((el, idx) =>
+                            <input id='childItem' className='form-control mb-2' onChange={(event) => getValue(event, "input", idx)} />)
+                    }
                 </div>
             </InputModal>
         </>
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,17 +1,24 @@
+import { useState } from 'react'
 import TreeView from '../components/TreeView'
 import ToolHead from '../components/ToolHead'
 import Data from '../public/Data.json'
-import { getAllBody, useAxios } from '../public/lib'
+import { getAllBody, Context } from '../public/lib'
 
 export default function App() {
+  const [context, setContext] = useState({})
   return (
-    <>
+    <Context.Provider value={[context, setContext]}>
       <ToolHead parentTitle={getAllBody(Data)} />
       <div className='TreePanel'>
         <ul>
           <TreeView data={Data} />
         </ul>
       </div>
-    </>
+      <div className='TreePanel'>
+        <ul>
+          <TreeView data={context} />
+        </ul>
+      </div>
+    </Context.Provider>
   )
 }

514841a0dfd63d0d2ff0bdcd47837babf2ff4115
silence <beast964089@gmail.com>
2022-09-08 12:20:52 +0800

    move folder

--- pages/api/DataPool.js
+++ pages/api/DataPool.js
@@ -6,7 +6,7 @@ export default function getData(req, res) {
   if (req.method === "GET") {
     if (!req.query.id) {
       let files = []
-      fs.readdir(process.cwd() + '/data_pool', (err, file) => {
+      fs.readdir(process.cwd() + '/public/data_pool', (err, file) => {
         if (err) {
           res.status(404).json({
             success: false,
@@ -23,9 +23,9 @@ export default function getData(req, res) {
         }
       })
     } else {
-      fs.readFile(process.cwd() + `/data_pool/${req.query.id}`, (err, data) => {
+      fs.readFile(process.cwd() + `/public/data_pool/${req.query.id}`, (err, data) => {
         if (err) {
-          console.log("Post File", process.cwd() + `/data_pool/${req.query.id}`)
+          console.log("Post File", process.cwd() + `/public/data_pool/${req.query.id}`)
           console.log(path.resolve())
           res.status(404).json({
             success: false,
@@ -47,7 +47,7 @@ export default function getData(req, res) {
     context['fileName'] = req.body.fileName
     context['dateTime'] = dayjs().format('YYYY/MM/DD HH:mm:ss')
     const jsonContent = JSON.stringify(context)
-    fs.writeFile(process.cwd() + `/data_pool/${req.body.fileName}.json`, jsonContent, 'utf-8', (err) => {
+    fs.writeFile(process.cwd() + `/public/data_pool/${req.body.fileName}.json`, jsonContent, 'utf-8', (err) => {
       if (err) {
         res.status(404).json({
           success: false,
